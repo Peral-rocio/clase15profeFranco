f0b441db6de91a258140e4180d851593ac51a014
Peralta Rocio <peralta.rocio.2229@gmail.com>
2025-09-12 21:28:07 -0300

    ejercicio 8

---
+++ 8.js
@@ -0,0 +1,9 @@
+//const prompt = require("prompt-sync")({ sigint: true });
+
+// Ejercicio 8: Extraer una parte de una frase
+// Tienes la frase "Los gatos son geniales". Extrae y muestra solo la parte "gatos" utilizando el método adecuado.
+
+let frase = "los gatos son geniales";
+
+
+console.log(frase.slice(4 , 9));
