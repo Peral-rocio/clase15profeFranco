9c7cd54730a7794545fe574abb1380a1338adbf9
Peralta Rocio <peralta.rocio.2229@gmail.com>
2025-09-12 21:13:42 -0300

    Ejercicio 5

---
+++ 5.js
@@ -0,0 +1,14 @@
+//const prompt = require("prompt-sync")({ sigint: true });
+
+// Ejercicio 5: Buscar la posición de un elemento
+// Tienes una lista de colores ["Rojo", "Verde", "Azul", "Amarillo"]. Encuentra y muestra el índice del color "Azul" en la lista.
+
+let colores = [
+    "rojo",
+    "verde",
+    "azul",
+    "amarillo",
+    "marron",
+]
+
+console.log(colores.indexOf("azul")); //devuelve 2.
