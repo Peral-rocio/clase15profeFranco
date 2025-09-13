7d10d8dfe48aa7a78cecf344e386503aa8405f08
Peralta Rocio <peralta.rocio.2229@gmail.com>
2025-09-12 21:31:29 -0300

    ejercicio 9

---
+++ 9.js
@@ -0,0 +1,12 @@
+//const prompt = require("prompt-sync")({ sigint: true });
+
+// Ejercicio 9: Eliminar espacios innecesarios
+// Tienes la siguiente cadena con espacios adicionales
+// " Hola, mundo! "
+// Elimina los espacios al inicio y al final y muestra la cadena resultante.
+
+let saludo =  "             hola, mundo!        ";
+
+
+console.log(saludo);
+console.log (saludo.trim());
