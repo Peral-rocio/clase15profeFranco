d3bf45cc3f2cf303bcd371784fdba614335a87c6
Peralta Rocio <peralta.rocio.2229@gmail.com>
2025-09-12 21:35:19 -0300

    Ejercicio 10

---
+++ 10.js
@@ -0,0 +1,10 @@
+//const prompt = require("prompt-sync")({ sigint: true });
+
+// Ejercicio 10: Reemplazar una palabra en una frase
+// Tienes la frase "Me gusta el invierno". Cambia la palabra "invierno" por "verano" y muestra la frase actualizada.
+
+let meGusta = "Me gusta mucho el invierno";
+
+console.log(meGusta);
+
+console.log(meGusta.replace("invierno" , "verano"));
