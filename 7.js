b800d7059262fa8378a62689e9af1e8b58e57b16
Peralta Rocio <peralta.rocio.2229@gmail.com>
2025-09-12 21:21:26 -0300

    ejercicio 7

---
+++ 7.js
@@ -0,0 +1,15 @@
+//const prompt = require("prompt-sync")({ sigint: true });
+
+// Ejercicio 7: Verificar si una lista contiene un elemento
+// Tienes una lista de comidas ["Pizza", "Hamburguesa", "Tacos"]. Verifica si "Sushi" está en la lista y muestra un mensaje que confirme si está o no.
+
+let menu = [
+    "milanesa",
+    "hambuerguesas",
+    "sopa",
+    "empanadas",
+    "tarta",
+    "pizza",
+]
+
+console.log (menu.includes("sushi")); //False
